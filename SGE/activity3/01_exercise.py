3612c141a35e554da6aecd572a9409998ebfae5a
ByOver03 <nahuelrobert1718@gmail.com>
2025-11-13 21:09:27 +0100

    Ejercicios para hacer SGE

---
+++ SGE/activity3/01_exercise.py
@@ -0,0 +1,10 @@
+#Develop a program to manage a list of employees. Each employee will be represented as a dictionary with the following fields:
+#name, positions (a tuple with several roles), and salary
+# Implement options to:
+#   Add a new employee
+#   Calculate the average salary
+#   Search employees by specific position
+# Handle exceptions for:
+#   Invalid input when adding an employee
+#   Attempting to search for nonexistent position
+#
