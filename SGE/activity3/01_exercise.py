5ee7006cf02a0949c03d7a5af368fd272cec0f85
ByOver03 <nahuelrobert1718@gmail.com>
2025-11-19 18:58:55 +0100

    Cambio en ejercicios

--- SGE/activity3/01_exercise.py
+++ SGE/activity3/01_exercise.py
@@ -1,10 +1,4 @@
-#Develop a program to manage a list of employees. Each employee will be represented as a dictionary with the following fields:
-#name, positions (a tuple with several roles), and salary
-# Implement options to:
-#   Add a new employee
-#   Calculate the average salary
-#   Search employees by specific position
-# Handle exceptions for:
-#   Invalid input when adding an employee
-#   Attempting to search for nonexistent position
-#
+inventory: dict(int | str, dict)= {
+    'id': 1,
+    'name': 'Laptop',
+    'details': (float(price), int (stock))}
