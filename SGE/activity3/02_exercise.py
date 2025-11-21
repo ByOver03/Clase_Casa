95c26a680cf9033a75748ba31a345264f821121a
ByOver03 <nahuelrobert1718@gmail.com>
2025-11-21 19:59:40 +0100

    Terminar Ejercicios 2 y 3, Y hacer Actividad 4

---
+++ SGE/activity3/02_exercise.py
@@ -0,0 +1,13 @@
+student ={}
+
+id = 0
+name = ""
+subjects =[]
+grades =[]
+
+
+#Añadir a un estudiante con sus materias y notas
+
+#Mostrar todos los estudiantes con sus materias y notas
+
+#Calcular el promedio de las notas de un alumno
